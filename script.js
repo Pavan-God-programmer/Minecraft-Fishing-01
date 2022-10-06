f5f333387826780b4f54b5a868a51ad871d66c58
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-06 11:23:27 -0600

    BRT

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Minecraft fishing Simlatior
+
+// Varibles to store HTML Elements
+let steveImgEl = document.getElementById("steve-img");
+let alexImgE1 = document.getElementById("alex-img");
+let fishBtnE1 = document.getElementById("fish-btn");
+let imgResultE1 = document.getElementById("img-result");
+let numcodE1 = document.getElementById("num-cod");
+let numSalmonE1 = document.getElementById("num-salmon");
+let numTropicalE1 = document.getElementById("num-tropical");
+let numPufferE1 = document.getElementById("num-puffer");
