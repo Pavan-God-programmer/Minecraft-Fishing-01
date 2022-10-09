b13932037bdad096e7a20ee492ca61c0182991c3
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-08 18:02:54 -0600

    Update script.js

--- script.js
+++ script.js
@@ -9,18 +9,41 @@ let numSalmonEl = document.getElementById("num-salmon");
 let numTropicalEl = document.getElementById("num-tropical");
 let numPufferEl = document.getElementById("num-puffer");
 
+// Global
+let character = "Steve";
+
 // Event listers
 steveImgEl.addEventListener("click", selectsteve);
 alexImgEl.addEventListener("click", selectAlex);
+fishBtnEl.addEventListener("click", fishOnce);
 
+// Event 
 function selectsteve() {
   // Up
   steveImgEl.classList.add("active");
   alexImgEl.classList.remove("active");
+
+  // up
+  character = "Steve";
 }
 
 function selectAlex() {
   // Up
   steveImgEl.classList.remove("active");
   alexImgEl.classList.add("active");
+  
+  // up
+  character = "Alex";
+}
+
+
+function fishOnce () {
+  // Test 
+  if  (character === "Steve") {
+    // use Steve
+    alert ("Fish with Steve")
+  } else {
+    // use Alex
+    alert ("Fish with Alex")
+  }
 }
