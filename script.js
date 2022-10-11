40dadf60cddc987c7cc920f49ea4b83b0c14310a
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-11 11:23:22 -0600

    qedwfrfe

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 // Minecraft fishing Simlatior
-
 // Varibles to store HTML Elements
 let steveImgEl = document.getElementById("steve-img");
 let alexImgEl = document.getElementById("alex-img");
@@ -10,59 +9,61 @@ let numSalmonEl = document.getElementById("num-salmon");
 let numTropicalEl = document.getElementById("num-tropical");
 let numPufferEl = document.getElementById("num-puffer");
 
-<<<<<<< HEAD
-// Globile 
-let character = "Steve"
-=======
 // Global
 let character = "Steve";
->>>>>>> b13932037bdad096e7a20ee492ca61c0182991c3
+let numcod = 0;
+let numSalmon = 0;
+let numTropical = 0;
+let numPuffer = 0;
 
 // Event listers
 steveImgEl.addEventListener("click", selectsteve);
 alexImgEl.addEventListener("click", selectAlex);
 fishBtnEl.addEventListener("click", fishOnce);
 
-// Event 
+// Event
 function selectsteve() {
   // Up
   steveImgEl.classList.add("active");
   alexImgEl.classList.remove("active");
 
-<<<<<<< HEAD
-  // Up
-   character = "Steve";
-=======
   // up
   character = "Steve";
->>>>>>> b13932037bdad096e7a20ee492ca61c0182991c3
 }
 
 function selectAlex() {
   // Up
   steveImgEl.classList.remove("active");
   alexImgEl.classList.add("active");
-<<<<<<< HEAD
 
-=======
-  
->>>>>>> b13932037bdad096e7a20ee492ca61c0182991c3
   // up
   character = "Alex";
 }
 
-
-<<<<<<< HEAD
-function fishOnce
-=======
-function fishOnce () {
-  // Test 
-  if  (character === "Steve") {
+function fishOnce() {
+  // Test
+  if (character === "Steve") {
     // use Steve
-    alert ("Fish with Steve")
+    let randNum = Math.random();
+    console.log(randNum);
+    if (randNum < 0.7) {
+      numcod++;
+      imgResultEl.src = "img/Raw-Cod.png";
+      numcodEl.innerHTML = numcod;
+    } else if (randNum < 0.9) {
+      numSalmon++;
+      imgResultEl.src = "img/Raw-Salmon.png";
+      numSalmonEl.innerHTML = numSalmon;
+    } else if (randNum < 0.95) {
+      numTropical++;
+      imgResultEl.src = "img/Tropical-Fish.png";
+      numTropicalEl.innerHTML = numTropical;
+    } else {
+      numPuffer++;
+      imgResultEl.src = "img/Pufferfish.png";
+      numPufferEl.innerHTML = numPuffer;
+    }
   } else {
     // use Alex
-    alert ("Fish with Alex")
   }
 }
->>>>>>> b13932037bdad096e7a20ee492ca61c0182991c3
