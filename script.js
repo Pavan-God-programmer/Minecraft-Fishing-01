f48013fdfd405762a3deef202f929efc63806d2d
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-12 11:02:01 -0600

    Done👍

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Minecraft fishing Simlatior
 // Varibles to store HTML Elements
 let steveImgEl = document.getElementById("steve-img");
+let villageImgEl = document.getElementById("villager-img");
 let alexImgEl = document.getElementById("alex-img");
 let fishBtnEl = document.getElementById("fish-btn");
 let imgResultEl = document.getElementById("img-result");
@@ -17,6 +18,7 @@ let numTropical = 0;
 let numPuffer = 0;
 
 // Event listers
+villageImgEl.addEventListener("click", selectVillager);
 steveImgEl.addEventListener("click", selectsteve);
 alexImgEl.addEventListener("click", selectAlex);
 fishBtnEl.addEventListener("click", fishOnce);
@@ -26,6 +28,7 @@ function selectsteve() {
   // Up
   steveImgEl.classList.add("active");
   alexImgEl.classList.remove("active");
+  villageImgEl.classList.remove("active");
 
   // up
   character = "Steve";
@@ -35,9 +38,20 @@ function selectAlex() {
   // Up
   steveImgEl.classList.remove("active");
   alexImgEl.classList.add("active");
+  villageImgEl.classList.remove("active");
 
   // up
   character = "Alex";
+}
+
+function selectVillager() {
+  // Up
+  steveImgEl.classList.remove("active");
+  alexImgEl.classList.remove("active");
+  villageImgEl.classList.add("active");
+
+  // up
+  character = "Villager";
 }
 
 function fishOnce() {
@@ -45,7 +59,9 @@ function fishOnce() {
   if (character === "Steve") {
     // use Steve
     let randNum = Math.random();
+
     console.log(randNum);
+
     if (randNum < 0.7) {
       numcod++;
       imgResultEl.src = "img/Raw-Cod.png";
@@ -65,5 +81,47 @@ function fishOnce() {
     }
   } else {
     // use Alex
+    let randNum = Math.random();
+
+    console.log(randNum);
+
+    if (randNum < 0.1) {
+      numcod++;
+      imgResultEl.src = "img/Raw-Cod.png";
+      numcodEl.innerHTML = numcod;
+    } else if (randNum < 0.2) {
+      numSalmon++;
+      imgResultEl.src = "img/Raw-Salmon.png";
+      numSalmonEl.innerHTML = numSalmon;
+    } else if (randNum < 0.5) {
+      numTropical++;
+      imgResultEl.src = "img/Tropical-Fish.png";
+      numTropicalEl.innerHTML = numTropical;
+    } else {
+      numPuffer++;
+      imgResultEl.src = "img/Pufferfish.png";
+      numPufferEl.innerHTML = numPuffer;
+    }
+  } // use Alex
+  let randNum = Math.random();
+
+  console.log(randNum);
+
+  if (randNum < 0.1) {
+    numcod++;
+    imgResultEl.src = "img/Raw-Cod.png";
+    numcodEl.innerHTML = numcod;
+  } else if (randNum < 0.1) {
+    numSalmon++;
+    imgResultEl.src = "img/Raw-Salmon.png";
+    numSalmonEl.innerHTML = numSalmon;
+  } else if (randNum < 0.2) {
+    numTropical++;
+    imgResultEl.src = "img/Tropical-Fish.png";
+    numTropicalEl.innerHTML = numTropical;
+  } else {
+    numPuffer++;
+    imgResultEl.src = "img/Pufferfish.png";
+    numPufferEl.innerHTML = numPuffer;
   }
 }
